cc2804fd5758c8dfa5b61ace0d7e025bcd22b96b
MayHyeyeonKim <hkim7963@gmail.com>
2024-06-10 06:54:47 -0700

    modified: header

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,6 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { userActions } from "../action/userAction";
 import * as types from "../constants/product.constants";
+import { Row, Col, Container } from "react-bootstrap";
 
 const Navbar = ({ user }) => {
   const dispatch = useDispatch();
@@ -44,62 +45,66 @@ const Navbar = ({ user }) => {
   };
 
   return (
-    <div>
-      {showSearchBox && (
-        <div className="display-space-between mobile-search-box w-100">
-          <div className="search display-space-between w-100">
-            <div>
-              <FontAwesomeIcon className="search-icon" icon={faSearch} />
-              <input
-                type="text"
-                placeholder="Search Products"
-                onKeyPress={onCheckEnter}
-              />
+    <>
+      <div>
+        {showSearchBox && (
+          <div className="display-space-between mobile-search-box w-100">
+            <div className="search display-space-between w-100">
+              <div>
+                <FontAwesomeIcon className="search-icon" icon={faSearch} />
+                <input
+                  type="text"
+                  placeholder="Search Products"
+                  onKeyPress={onCheckEnter}
+                />
+              </div>
+              <button
+                className="closebtn"
+                onClick={() => setShowSearchBox(false)}
+              >
+                &times;
+              </button>
             </div>
-            <button
-              className="closebtn"
-              onClick={() => setShowSearchBox(false)}
-            >
-              &times;
-            </button>
+          </div>
+        )}
+        <div className="side-menu" style={{ width: width }}>
+          <button className="closebtn" onClick={() => setWidth(0)}>
+            &times;
+          </button>
+
+          <div className="side-menu-list" id="menu-list">
+            {menuList.map((menu, index) => (
+              <button key={index}>{menu}</button>
+            ))}
           </div>
         </div>
-      )}
-      <div className="side-menu" style={{ width: width }}>
-        <button className="closebtn" onClick={() => setWidth(0)}>
-          &times;
-        </button>
-
-        <div className="side-menu-list" id="menu-list">
-          {menuList.map((menu, index) => (
-            <button key={index}>{menu}</button>
-          ))}
-        </div>
-      </div>
-      {user && user.level === "admin" && (
-        <Link to="/admin/product?page=1" className="link-area">
-          Admin page
-        </Link>
-      )}
-      <div className="nav-header">
-        <div className="burger-menu hide">
-          <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
-        </div>
-
-        <div>
-          <div className="display-flex">
+        {user && user.level === "admin" && (
+          <Link to="/admin/product?page=1" className="link-area">
+            Admin page
+          </Link>
+        )}
+        <div className="nav-header d-flex justify-content-between align-items-center">
+          <div className="d-flex align-items-center">
+            <div className="burger-menu hide">
+              <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
+            </div>
+            {user && (
+              <div className="welcome-message">
+                Welcome, {user.name}!
+              </div>
+            )}
+          </div>
+          <div className="d-flex align-items-center ml-auto">
             {user ? (
               <>
-              <div className="nav-icon welcome-message">
-              Welcome, {user.name}!
-            </div>
-              <div onClick={logout} className="nav-icon">
-                <FontAwesomeIcon icon={faUser} />
-                {!isMobile && (
-                  <span style={{ cursor: "pointer" }}>Logout</span>
-                )}
-              </div>
-              </>) : (
+                <div onClick={logout} className="nav-icon">
+                  <FontAwesomeIcon icon={faUser} />
+                  {!isMobile && (
+                    <span style={{ cursor: "pointer" }}>Logout</span>
+                  )}
+                </div>
+              </>
+            ) : (
               <div onClick={() => navigate("/login")} className="nav-icon">
                 <FontAwesomeIcon icon={faUser} />
                 {!isMobile && <span style={{ cursor: "pointer" }}>Login</span>}
@@ -127,33 +132,33 @@ const Navbar = ({ user }) => {
             )}
           </div>
         </div>
-      </div>
 
-      <div className="nav-logo">
-        <Link to="/">
-          <img width={350} src="/image/mayday_logo.png" alt="mayday_logo.png" />
-        </Link>
+        <div className="nav-logo">
+          <Link to="/">
+            <img width={350} src="/image/mayday_logo.png" alt="mayday_logo.png" />
+          </Link>
+        </div>
+        <div className="nav-menu-area">
+          <ul className="menu">
+            {menuList.map((menu, index) => (
+              <li key={index}>
+                <a href="#">{menu}</a>
+              </li>
+            ))}
+          </ul>
+          {!isMobile && (
+            <div className="search-box landing-search-box ">
+              <FontAwesomeIcon icon={faSearch} />
+              <input
+                type="text"
+                placeholder="search"
+                onKeyPress={onCheckEnter}
+              />
+            </div>
+          )}
+        </div>
       </div>
-      <div className="nav-menu-area">
-        <ul className="menu">
-          {menuList.map((menu, index) => (
-            <li key={index}>
-              <a href="#">{menu}</a>
-            </li>
-          ))}
-        </ul>
-        {!isMobile && (
-          <div className="search-box landing-search-box ">
-            <FontAwesomeIcon icon={faSearch} />
-            <input
-              type="text"
-              placeholder="search"
-              onKeyPress={onCheckEnter}
-            />
-          </div>
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
