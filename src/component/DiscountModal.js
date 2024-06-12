8571b64eda3df31685c741d5b77264efd145c29d
MayHyeyeonKim <hkim7963@gmail.com>
2024-06-12 01:31:21 -0700

    chore

--- src/component/DiscountModal.js
+++ src/component/DiscountModal.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
-import '../discountModal.style.css'; // 스타일을 위한 CSS 파일을 생성합니다.
+import '../discountModal.style.css';
 
 const DiscountModal = ({ show, handleClose }) => {
     return (
-        <Modal show={show} onHide={handleClose} centered size="xl">
+        <Modal show={show} onHide={handleClose} centered size="xl" >
             <div className="modal-close-button" onClick={handleClose}>
                 &times;
             </div>
-            <Modal.Body>
+            <Modal.Body className="discount-modal-body">
                 <Row>
                     <Col md={6} className="p-0">
                         <img 
-                            src="https://res.cloudinary.com/db83rk1et/image/upload/v1718020189/kliedewhjfxlmtesmmwo.jpg" // 실제 이미지 경로로 변경하세요
+                            src="https://res.cloudinary.com/db83rk1et/image/upload/v1718020189/kliedewhjfxlmtesmmwo.jpg"
                             alt="Discount"
-                            className="img-fluid h-100 w-100" 
+                            className="img-fluid" 
+                            
                         />
                     </Col>
                     <Col md={6} className="p-4">
